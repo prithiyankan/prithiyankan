13f3e25b668af04259ee8c55448761d95ef8ea91
prithiyankan <adhiprithiyankan@icloud.com>
2024-09-12 16:15:11 +0530

    Add files via upload
    Python Program of Currency Convertor

---
+++ prithiyankan.py
@@ -0,0 +1,21 @@
+rupee=int(input('Enter rupees'))
+dollar =rupee*0.012
+jpy=rupee*1.70
+cny=rupee*0.085
+gbp=rupee*0.0091
+aed=rupee*0.044
+dem=rupee*0.011
+lkr=rupee*3.59
+sgd=rupee*0.016
+pkr=rupee*3.32
+sar=rupee*0.045
+print(f'the{rupee}in Dollars are {dollar}')
+print(f'the{rupee}in Japanese yen are {jpy}')
+print(f'the{rupee}in Chinese Yuan are {cny}')
+print(f'the{rupee}in Pound sterling are {gbp}')
+print(f'the{rupee}in United Arab Emirates Dirham are {aed}')
+print(f'the{rupee}in Germany Currency are {dem}')
+print(f'the{rupee}in Srilankan Rupee are {lkr}')
+print(f'the{rupee}in Singapore Dollar are {sgd}')
+print(f'the{rupee}in Pakistani rupee are {pkr}')
+print(f'the{rupee}in Saudi Arabian Riyals are {sar}')
